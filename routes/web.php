08c9bb10d36160fe2167c677dacd6e488e3dea40
unknown <arthurcavani@gmail.com>
2023-12-01 22:08:36 -0300

    Agrupamento e redirecionamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function () {
+    return 'login';
+})->name('site.login');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'clientes';
+    })->name('app.clientes');
+    Route::get('/fornecedores', function () {
+        return 'fornecedores';
+    })->name('app.fornecedores');
+    Route::get('/produtos', function () {
+        return 'produtos';
+    })->name('app.produtos');
+});
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/rota1', function(){
+    echo 'Rota 1';
+})->name('site.rota1');
 
-Route::get('/contato/{nome}/{categoria_id}', 
-    function(string $nome = 'Desconhecido', int $categoria_id = 1) {
-    echo "Estamos aqui: $nome - $categoria_id";
-})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+// Route::redirect('/rota2', '/rota1');
