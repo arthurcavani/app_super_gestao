7c598e0b26040cfb057f033a7e990217a26d2ff9
unknown <arthurcavani@gmail.com>
2023-12-14 21:39:10 -0300

    Validando formularios

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,27 +7,26 @@ use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato (Request $request) {
-        // echo "<pre>";
-        // print_r($request->all());
-        // echo "</pre>";
-        // echo $request->input('nome');
-        // echo '<br>';
-        // echo $request->input('email');
+    public function contato(Request $request)
+    {
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+        return view('site.contato', ['titulo' => 'Contato - teste', 'motivo_contatos' => $motivo_contatos]);
+    }
 
-        // $contato = new SiteContato();
-        // $contato->nome = $request->input('nome');
-        // $contato->telefone = $request->input('telefone');
-        // $contato->email = $request->input('email');
-        // $contato->motivo_contato = $request->input('motivo_contato');
-        // $contato->mensagem = $request->input('mensagem');
-
-        // print_r($contato->getAttributes());
-        // $contato->save();
-
-        $contato = new SiteContato();
-        $contato->create($request->all());
-        // print_r($contato->getAttributes());
-        return view('site.contato', ['titulo' => 'Contato - teste']);
+    public function salvar(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
+        
+        // SiteContato::create($request->all());
     }
 }
