f7b012d6c23b692c87a7d76f604f87aeb8b2ad05
unknown <arthurcavani@gmail.com>
2023-12-16 12:11:40 -0300

    Novas migrations para preencher motivo_contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -4,16 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\SiteContato;
+use App\MotivoContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request)
     {
-        $motivo_contatos = [
-            '1' => 'Dúvida',
-            '2' => 'Elogio',
-            '3' => 'Reclamação'
-        ];
+        $motivo_contatos = MotivoContato::all();
         return view('site.contato', ['titulo' => 'Contato - teste', 'motivo_contatos' => $motivo_contatos]);
     }
 
@@ -22,7 +19,7 @@ class ContatoController extends Controller
         $request->validate([
             'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
-            'email' => 'required',
+            'email' => 'email',
             'motivo_contato' => 'required',
             'mensagem' => 'required|max:2000'
         ]);
