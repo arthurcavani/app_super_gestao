4baa99d10abba7ba31c758c23d32d7a0796f3afa
unknown <arthurcavani@gmail.com>
2024-01-31 21:09:11 -0300

    Relacionamento N pra N de produtos e pedidos

--- resources/views/app/pedido/index.blade.php
+++ resources/views/app/pedido/index.blade.php
@@ -25,6 +25,8 @@
                             <th>Cliente</th>
                             <th></th>
                             <th></th>
+                            <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -32,6 +34,7 @@
                             <tr>
                                 <td>{{ $pedido->id }}</td>
                                 <td>{{ $pedido->cliente_id }}</td>
+                                <td><a href="{{ route('pedido-produto.create', ['pedido' => $pedido->id]) }}">Adicionar Produtos</a></td>
                                 <td><a href="{{ route('pedido.show', ['pedido' => $pedido->id]) }}">Visualizar</a></td>
                                 <td>
                                     <form id="form_{{$pedido->id}}" method="post" action="{{ route('pedido.destroy', ['pedido' => $pedido->id]) }}">
